38a58c4f88f06f200ed757a36576917ce9fae8a1
Newton <altontonnalumasa@gmail.com>
2022-12-24 02:01:14 +0300

    add two functions

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -5,6 +5,8 @@ import maleProfile from "./images/maleProfile.jpg";
 
 const Employees = () => {
   // eslint-disable-next-line
+  const [selectedTeam, setTeam] = useState("TeamB")
+  // eslint-disable-next-line
   const [employees, setEmployees] = useState([
     {
       id: 1,
@@ -91,11 +93,22 @@ const Employees = () => {
       teamName: "TeamD",
     },
   ]);
+
+  function handleTeamSelectionChange(event) {
+    console.log(event.target.value);
+    setTeam(event.target.value);
+  }
+  function handleEmployeeCardClick(event) {
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
+    ? (employee.teamName === selectedTeam)?{...employee, teamName: ""}:{...employee, teamName: selectedTeam}: employee)
+    setEmployees(transformedEmployees);
+  }
+
   return (
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-6">
-          <select className="form-select form-select-lg">
+          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
             <option value="TeamA">Team A</option>
             <option value="TeamB">Team B</option>
             <option value="TeamC">Team C</option>
@@ -107,31 +120,16 @@ const Employees = () => {
         <div className="col-8">
           <div className="card-collection">
             {employees.map((employee) => (
-              <div
-                id={employee.id}
-                className="card m-2"
-                style={{ cursor: "pointer" }}
-              >
-                {employee.gender === "male" ? (
-                  <img
-                    src={maleProfile}
-                    className="card-img-top"
-                    alt="male profile"
-                  />
-                ) : (
-                  <img
-                    src={femaleProfile}
-                    className="card-img-top"
-                    alt="male profile"
-                  />
-                )}
+            <div id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
+              {employee.gender === "male" ? (
+                <img src={maleProfile}  className="card-img-top" alt="male profile" />) : (<img src={femaleProfile} className="card-img-top" alt="male profile"/>)}
                 <div className="card-body">
                   <h5 className="card-title">Full Name: {employee.fullName}</h5>
                   <p className="card-text">
                     <b>Designation:</b> {employee.designation}
                   </p>
                 </div>
-              </div>
+            </div>
             ))}
           </div>
         </div>
